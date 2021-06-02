1c7865fa35177e8ec39315e769b0a5d697e8d047
admin <admin@YD03-09>
2021-06-02 17:46:23 +0900

    5일차 과제 피드백

--- 4일차/src/co/yedam/member/Rent.java
+++ 4일차/src/co/yedam/member/Rent.java
@@ -23,18 +23,19 @@ public class Rent {
 	}
 
 	
-	public Rent(String title, String name, String date) {
+	public Rent(String title, String name, String date) {		//매개변수 3개짜리 생성자
 		super();
 		this.title = title;
 		this.name = name;
 		this.date = date;
 	}
 	
-	public Rent(String title, String name) {
-		this(title, name);
+	public Rent(String title, String name) {					//매개변수 2개짜리 생성자
+		this(title, name, "2021-6-2");							//근데 이제 2개짜리 생성자는 받아올 수 있는 게 없으니까????밑에서 저 형식 받아옴
+																//this(어쩌구저쩌구) 형식일 때
 	}
 	
-	public Rent() {
+	public Rent() {												//매개변수 안 받는 생성자
 		this("","","");
 	}
 	
--- 4일차/src/co/yedam/member/Service.java
+++ 4일차/src/co/yedam/member/Service.java
@@ -1,9 +1,14 @@
 package co.yedam.member;
 
 public interface Service {
+	//등록
 	void input();
+	//수정
 	void update();
+	//삭제
 	void delete();
+	//전체조회
 	void selectAll();
+	//단건조회
 	void selectOne();
 }
